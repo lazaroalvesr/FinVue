820a24c2b6a30e46321490f55a7f4fa8e088459e
lazaro <lazaroalves12355@gmail.com>
2024-02-01 12:54:30 -0300

    ajustes bug mobile

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,4 +1,3 @@
-// Importando as dependências necessárias
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
@@ -6,26 +5,21 @@ import LoginButton from "./LoginButton";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { CiMenuFries } from "react-icons/ci";
 
-// Definindo o componente de cabeçalho
 export default function Header() {
-  // Utilizando o hook useUser para obter informações sobre o usuário autenticado
   const { user, error, isLoading } = useUser();
 
-  // Estado para controlar o menu de navegação
   const [ativaMenu, setAtivaMenu] = useState(false);
 
-  // Função para alternar a visibilidade do menu
   function toggle() {
     setAtivaMenu((ativaMenu) => !ativaMenu);
   }
 
-  // Renderização condicional com base no estado do usuário
   return (
-    <div className="w-full border-b border-neutral-300">
-      <header className="pb-3 lg:max-w-6xl m-auto mt-4 flex lg:items-center lg:justify-between">
-        <div className="flex w-32 items-center lg:justify-between">
-          <div>
-            <Link href="/" className="flex items-center">
+    <div className="">
+      <header className="pb-3 lg:max-w-6xl lg:m-auto mt-4 flex lg:items-center lg:justify-between ">
+        <div className="flex  lg:justify-between ">
+          <div className="flex ">
+            <Link href="/" className="flex items-center ">
               <Image
                 src="/img/icone.png"
                 alt="Icone Money"
@@ -34,14 +28,14 @@ export default function Header() {
               />
               <p className="pl-2 text-2xl">FinVue</p>
             </Link>
+            <div className="lg:hidden absolute right-5 top-7">
+              <button onClick={() => toggle()}>
+                <CiMenuFries />
+              </button>
+            </div>
           </div>
-          <div className="lg:hidden absolute right-6">
-            <button onClick={() => toggle()}>
-              <CiMenuFries />
-            </button>
-          </div>
-          <div className="lg:ml-12 pl-4 items-center pt-6">
-            <ul className={`flex gap-4 lg:static lg:h-auto ${ativaMenu ? 'absolute bg-gray-50 left-0 w-full text-center top-20 h-full pt-32 flex-col z-50' : "h-0 overflow-hidden"}`}>
+          <div className="lg:ml-12 ml-0 pl-4 items-center pt-6 relative">
+            <ul className={`lg:flex gap-4 lg:static lg:h-auto ${ativaMenu ? 'absolute bg-gray-50 left-0 w-full text-center top-20 h-full pt-32 flex-col z-50' : "h-0 overflow-hidden"}`}>
               <li className="text-3xl lg:text-sm">
                 <Link href={'/'}>
                   Personal
@@ -57,14 +51,8 @@ export default function Header() {
                   Pricing
                 </Link>
               </li>
-              <li className="text-3xl lg:text-sm">
-                <Link href={'/private'}>
-                  Private
-                </Link>
-              </li>
               <div className="lg:ml-[440px] ml-0">
                 {user ? (
-                  // Se o usuário estiver autenticado, exibe o nome e o botão de logout
                   <>
                     <div className="flex lg:flex-row flex-col">
                       <p className="pr-4 lg:pb-0 pb-4">Bem-vindo, {user.nickname}!</p>
@@ -72,7 +60,6 @@ export default function Header() {
                     </div>
                   </>
                 ) : (
-                  // Se o usuário não estiver autenticado, exibe o botão de login
                   <LoginButton url="/login" name="Login" />
                 )}
               </div>
@@ -83,3 +70,4 @@ export default function Header() {
     </div>
   );
 }
+
--- pages/components/Pricing.tsx
+++ pages/components/Pricing.tsx
@@ -8,10 +8,10 @@ export default function Pricing() {
         <div className="lg:ml-80 ml-8">
           <p className="bg-blue-100 lg:ml-80 p-2 mb-5 rounded-md w-16">Preço</p>
           <h1 className="text-5xl font-semibold">Planos para todos os orçamentos</h1>
-          <p className="lg:w-[500px] lg:text-center text-start mt-6 lg:ml-32 w-80">Quer você seja uma pequena empresa ou uma grande corporação, temos o plano certo para você</p>
+          <p className="lg:w-[500px] lg:text-center text-start mt-6 lg:ml-32 w-72">Quer você seja uma pequena empresa ou uma grande corporação, temos o plano certo para você</p>
         </div>
         <div className="mt-20 flex lg:flex-row gap-12 flex-col  mx-2 lg:mx-0 ">
-          <div className="lg:w-96 w-[360px] bg-gray-50 p-8 rounded-xl drop-shadow-md">
+          <div className="lg:w-96 w-[330px] bg-gray-50 p-8 rounded-xl drop-shadow-md">
             <h1 className="text-3xl pb-4 text-blue-500">Pessoal</h1>
             <p className="pb-7">Orçamento integrado e aproveite melhor seu dinheiro com sua conta padrão</p>
             <p><span className="text-blue-500 text-3xl">R$ 0</span> / mês</p>
@@ -26,7 +26,7 @@ export default function Pricing() {
           <div className="lg:w-[760px] mx-2 lg:mx-0 relative flex lg:flex-row flex-col text-gray-50 bg-blue-500 p-8 rounded-xl drop-shadow-md">
             <div>
               <h1 className="text-3xl pb-4 ">Plus</h1>
-              <p className="pb-7 w-80">Orçamento integrado e aproveite melhor seu dinheiro com sua conta plus</p>
+              <p className="pb-7 lg;w-80">Orçamento integrado e aproveite melhor seu dinheiro com sua conta plus</p>
               <p><span className="-500 text-3xl">R$ 20</span> / mês</p>
               <button className=" mt-12 flex 
                 items-center justify-center
